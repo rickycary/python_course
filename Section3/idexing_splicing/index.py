6e23e3d658143cfdda807f88c14ec8b087673050
Richard <ricky.cary1@gmail.com>
2023-06-08 12:28:54 -0500

    String Indexing

---
+++ Section3/idexing_splicing/index.py
@@ -0,0 +1,14 @@
+# Indexing and Splicing with Strings
+
+myString = "Hello World"
+
+print(myString[0])
+print(myString[8])
+print(myString[9])
+print(myString[-2])
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+print(letters)
+print(letters[2:])
+print(letters[:4])
+print(letters[3:10])
