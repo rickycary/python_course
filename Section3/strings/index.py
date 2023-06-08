d99cd4f796974b89262effcdabb1a457181ce193
Richard <ricky.cary1@gmail.com>
2023-06-08 12:06:59 -0500

    Initial Commit

---
+++ Section3/strings/index.py
@@ -0,0 +1,11 @@
+# String basics
+string = 'hello'
+string2 = 'world'
+string3 = 'this is also a string'
+dailyActivity = "I'm going on a run"
+
+print(string)
+print(string2)
+print(string3)
+print(dailyActivity)
+len('hello')
