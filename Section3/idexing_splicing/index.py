3191ef9ad31f202aceae788b941c02617405498c
Richard <ricky.cary1@gmail.com>
2023-06-08 12:46:18 -0500

    String Properties

--- Section3/idexing_splicing/index.py
+++ Section3/idexing_splicing/index.py
@@ -12,3 +12,12 @@ print(letters)
 print(letters[2:])
 print(letters[:4])
 print(letters[3:10])
+
+# Use string slicing to grab the word 'ink'  from inside 'tinker' 
+# For example, 'education'[3:6]  returns 'cat' 
+# Remember that when slicing you only go up to but not including the end index.
+# You should only write one line of code for this. Do not assign a variable name to the string.
+
+stringSplicing = "tinker"
+print(stringSplicing[1:4])
+
---
+++ Section3/string_properties_and_methods/index.py
@@ -0,0 +1,16 @@
+x = 'Hello World'
+y = ' it is beautiful outside!'
+
+print(x + y)
+
+x = x + " it is beautiful outside!"
+
+print(x)
+
+letter = 'z' 
+print(letter * 10)
+
+print(x.upper())
+print(letter.upper())
+
+print(x.split())
