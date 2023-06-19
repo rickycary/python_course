9f1dafbc24b8d439603585b2de08d32946bdb9e5
Richard <ricky.cary1@gmail.com>
2023-06-19 11:46:30 -0500

    Lists in Python

---
+++ Section3/20/20.py
@@ -0,0 +1,22 @@
+my_list = [1, 2, 3]
+my_list = ['STRING', 100, 23.2]
+len(my_list)
+print(my_list)
+myList = ['one', 'two', 'three']
+print(myList[0])
+print(myList + my_list)
+
+myList.append('Six')
+print(myList)
+
+print(myList.pop())
+
+popped_item = my_list.pop()
+print(popped_item)
+
+new_list = ['a', 'b', 'c', 'd', 'x', 'e']
+print(new_list)
+new_list.sort()
+print(new_list)
+new_list.reverse()
+print(new_list)
