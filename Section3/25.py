dccd8c26323ba015688c57168d2d2fd4f25d148b
Richard <ricky.cary1@gmail.com>
2023-06-19 12:42:03 -0500

    Tuples with Python

---
+++ Section3/25.py
@@ -0,0 +1,15 @@
+t = (1,2,3)
+mylist = [1,2,3]
+print(type(t))
+print(len(t))
+
+t = ('one',2)
+print(t[-1])
+
+t = ('a', 'a', 'b')
+print(t.count('a'))
+print(t.index('a'))
+
+mylist[0] = 'New'
+print(mylist)
+
