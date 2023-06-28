0a1a99c02e6682c9dd77700c578da555641acf1a
Richard <ricky.cary1@gmail.com>
2023-06-28 11:02:59 -0500

    If Elif and Else Statements in Python

---
+++ section5/34.py
@@ -0,0 +1,32 @@
+# Example #1
+if 1>2:
+    print('Its True!')
+else:
+    print('Its not true')
+    
+# Example #2
+hungry = False
+if hungry:
+    print('Feed Me More!!!')
+else: 
+    print('Im not hungry')
+    
+# Example #3
+loc = 'Game'
+if loc == 'Auto Shop':
+    print('Cars are cool')
+elif loc == 'Bank':
+    print('Money is life')
+elif loc == 'Store':
+    print('Welcome to the store')
+else:
+    print('I do not know much')
+    
+# Example #4
+name = 'Ricky'
+if name == 'Frankie':
+    print('Hello Frankie')
+elif name == 'Sammy':
+    print('Hello Sammy')
+else:
+    print('What is your name?')
